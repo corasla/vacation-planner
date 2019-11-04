f6566b527cc4a577c2b76a9260c8b2abff0e2b17
corasla <milearzv@gmail.com>
2019-11-04 19:16:41 +0200

    Refactor place model to use IPlace interface and make constructor signature easier to use

--- src/app/places/models/place.model.ts
+++ src/app/places/models/place.model.ts
@@ -1,5 +1,14 @@
+export interface IPlace {
+  id?: number
+  name: string
+  description: string
+  country: string
+  city: string
+  image?: string
+  country_code?: string
+}
 
-export class Place {
+export class Place implements IPlace{
   id?: number
   name: string
   description: string
@@ -8,7 +17,17 @@ export class Place {
   image?: string
   country_code?: string
 
-  constructor(name: string, description: string, country: string, city: string, country_code: string = '', image: string = '', id: number = null) {
+  constructor(data: IPlace) {
+    const {
+      name,
+      country,
+      country_code,
+      city,
+      description,
+      image,
+      id,
+    } = data
+
     this.name = name
     this.country = country
     this.description = description
