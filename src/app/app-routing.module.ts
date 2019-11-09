45e46f810ccb47cf9ce05ca1b6ff9546b2bb5b7b
corasla <milearzv@gmail.com>
2019-11-09 11:12:39 +0200

    Implement router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AllPlacesComponent } from './places/components/all-places/all-places.component';
+import { UpcomingVacationsComponent } from './places/components/upcoming-vacations/upcoming-vacations.component';
+import { PastVacationsComponent } from './places/components/past-vacations/past-vacations.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'all', component: AllPlacesComponent },
+  { path: 'upcoming', component: UpcomingVacationsComponent },
+  { path: 'past', component: PastVacationsComponent },
+  { 
+    path: '',
+    redirectTo: '/all',
+    pathMatch: 'full'
+  },
+  { 
+    path: '**',
+    redirectTo: '/all',
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
