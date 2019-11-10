eee20e3690d98b38195e1cb052f24b8360015981
corasla <milearzv@gmail.com>
2019-11-10 19:34:35 +0200

    fix spec fail

--- src/app/services/auth-interceptor.service.spec.ts
+++ src/app/services/auth-interceptor.service.spec.ts
@@ -1,9 +1,12 @@
 import { TestBed } from '@angular/core/testing';
 
 import { AuthInterceptorService } from './auth-interceptor.service';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('AuthInterceptorService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    imports: [RouterTestingModule]
+  }));
 
   it('should be created', () => {
     const service: AuthInterceptorService = TestBed.get(AuthInterceptorService);
